54c8f8dd9ea1d861c868b52a48941148726afec6
root <root@MSI>
2023-11-21 14:13:35 -0600

    the fuck am i doing wrong

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,30 +10,30 @@ import sys
 
 def export_to_csv():
     """
-    exports to csv
+    data grabber
     """
-    if len(sys.argv) != 2:
-        print("Error: Please provide a user ID as a command-line argument.")
-        sys.exit(1)
+    if(len(sys.argv) != 2):
+        print("Error not 3 commands")
 
     USER_ID = sys.argv[1]
 
-    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)).json()
-    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos', params={"userId": USER_ID}).json()
-
+    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(USER_ID)).json()
+    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params={"userId": USER_ID}).json()
     EMPLOYEE_UN = user_data.get("username")
     EMPLOYEE_NAME = user_data.get("name")
-
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
+
     completed_tasks = []
 
     with open('{}.csv'.format(USER_ID), 'w') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
         for item in todo_data:
-            writer.writerow([USER_ID, EMPLOYEE_UN, item.get("completed"), item.get("title")])
-
+            writer.writerow((USER_ID, EMPLOYEE_UN,
+                             item.get("completed"), item.get("title")))
+            
             TOTAL_NUMBER_OF_TASKS += 1
             if item.get("completed"):
                 NUMBER_OF_DONE_TASKS += 1
@@ -42,7 +42,8 @@ def export_to_csv():
     print('Employee {} is done with tasks({}/{}):'
           .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for task in completed_tasks:
-        print("\t{}".format(task))
+        print("\t {}".format(task))
+
 
 if __name__ == "__main__":
     export_to_csv()
--- api/main.py
+++ api/main.py
@@ -12,24 +12,25 @@ todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
 def user_info(id):
-    """ Check user information """
+    """ Check CSV formatting """
 
-    total_tasks = 0
     response = requests.get(todos_url).json()
-    for i in response:
-        if i['userId'] == id:
-            total_tasks += 1
-
-    num_lines = 0
     with open(str(id) + ".csv", 'r') as f:
-        for line in f:
-            if not line == '\n':
-                num_lines += 1
+        output = f.read().strip()
+        count = 0
+        flag = 0
+        for i in response:
+            if i['userId'] == id:
+                url = users_url + str(i['userId'])
+                usr_resp = requests.get(url).json()
+                line = '"' + str(i['userId']) + '","' + usr_resp[0]['username'] + '","' + str(i['completed']) + '","' + i['title'] + '"'
+                count += 1
+                if not line in output:
+                    print("Task {} Formatting: Incorrect".format(count))
+                    flag = 1
 
-    if total_tasks == num_lines:
-        print("Number of tasks in CSV: OK")
-    else:
-        print("Number of tasks in CSV: Incorrect")
+    if flag == 0:
+        print("Formatting: OK")
 
 
 if __name__ == "__main__":

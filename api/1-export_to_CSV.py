d7e4ed930519b50bcafa1cc4546ffa3a93dd849c
root <root@MSI>
2023-11-21 13:49:23 -0600

    changes

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python3
 """
-export to csv
+csv export
 """
 
 import csv
-from requests import get
-from sys import argv
-
-url_base = 'https://jsonplaceholder.typicode.com/users/'
+import requests
+import sys
 
 
-def export_csv():
-    user_data = get(url_base + argv[1]).json()
-    tasks_data = get(url_base + argv[1] + '/todos').json()
-    file_name = argv[1] + '.csv'
+def export_to_csv():
+    """
+    data grabber
+    """
+    if(len(sys.argv) != 2):
+        print("Error not 3 commands")
 
-    with open(file_name, 'w', encoding='utf-8', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quotechar='"',
-                                quoting=csv.QUOTE_MINIMAL)
+    USER_ID = sys.argv[1]
 
-        for task in tasks_data:
-            csv_writer.writerow([user_data['id'], user_data['username'],
-                                 task['completed'], task['title']])
+    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(USER_ID)).json()
+    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params={"userId": USER_ID}).json()
+    EMPLOYEE_UN = user_data.get("username")
+    EMPLOYEE_NAME = user_data.get("name")
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+
+    completed_tasks = []
+
+    with open('{}.csv'.format(USER_ID), 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for item in todo_data:
+            writer.writerow((USER_ID, EMPLOYEE_UN,
+                             item.get("completed"), item.get("title")))
+            
+            TOTAL_NUMBER_OF_TASKS += 1
+            if item.get("completed"):
+                NUMBER_OF_DONE_TASKS += 1
+                completed_tasks.append(item.get("title"))
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for task in completed_tasks:
+        print("\t {}".format(task))
 
 
-if __name__ == '__main__':
-    export_csv()
+if __name__ == "__main__":
+    export_to_csv()

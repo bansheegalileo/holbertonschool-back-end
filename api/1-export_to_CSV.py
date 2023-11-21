c37418f8a1c4bff5ac7e11a77a5619650c42e3e8
root <root@MSI>
2023-11-21 14:34:32 -0600

    sfgseg

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,49 +1,56 @@
 #!/usr/bin/python3
 """
-csv export
+Worker drone surveillance sim
 """
 
 import csv
 import requests
-import sys
+from sys import argv
+
+URL_BASE = 'https://jsonplaceholder.typicode.com/users/'
 
 
-def export_to_csv():
+def get_data():
     """
-    data grabber
+    and export to csv
     """
-    if(len(sys.argv) != 2):
-        print("Error not 3 commands")
+    user_id = argv[1]
 
-    USER_ID = sys.argv[1]
+    user_data = requests.get(URL_BASE + user_id).json()
+    username = user_data['username']
 
-    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(USER_ID)).json()
-    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
-                             params={"userId": USER_ID}).json()
-    EMPLOYEE_UN = user_data.get("username")
-    EMPLOYEE_NAME = user_data.get("name")
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+    todos = requests.get(URL_BASE + user_id + '/todos/').json()
 
-    completed_tasks = []
+    csv_file_name = '{}.csv'.format(user_id)
+    with open(csv_file_name, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow([
+            "USER_ID",
+            "USERNAME",
+            "TASK_COMPLETED_STATUS",
+            "TASK_TITLE"
+        ])
 
-    with open('{}.csv'.format(USER_ID), 'w') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for item in todo_data:
-            writer.writerow((USER_ID, EMPLOYEE_UN,
-                             item.get("completed"), item.get("title")))
+        for item in todos:
+            task_completed_status = "True" if item['completed'] else "False"
+            task_title = item['title']
 
-            TOTAL_NUMBER_OF_TASKS += 1
-            if item.get("completed"):
-                NUMBER_OF_DONE_TASKS += 1
-                completed_tasks.append(item.get("title"))
+            csv_writer.writerow([
+                user_id,
+                username,
+                task_completed_status,
+                task_title
+            ])
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in completed_tasks:
-        print("\t {}".format(task))
+    print(
+        'Employee {} is done with tasks({}/{}):\n{}'.format(
+            user_data['name'],
+            sum(1 for item in todos if item['completed']),
+            len(todos),
+            csv_file_name
+        )
+    )
 
 
-if __name__ == "__main__":
-    export_to_csv()
+if __name__ == '__main__':
+    get_data()
